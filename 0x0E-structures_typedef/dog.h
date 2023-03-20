0ab76d04d3b8ad58991e50f0ae1f84a8c6925651
joeybanknote <zrox759@gmail.com>
2023-03-20 13:14:21 -0700

    doggo

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,4 @@
+#ifndef DOG_H
 #define DOG_H
 
 /**
@@ -13,13 +14,12 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-	};
+};
 
 /**
  * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
-
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
